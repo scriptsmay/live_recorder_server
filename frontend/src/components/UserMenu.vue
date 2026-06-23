6791b32fa6ba199f5e4b3cdc1406bfb11627c6e2
scriptsmay <scriptsmay@163.com>
2026-06-23 15:44:38 +0800

    fix(ui): add stop modifier to change password click to prevent details toggle

--- frontend/src/components/UserMenu.vue
+++ frontend/src/components/UserMenu.vue
@@ -31,7 +31,7 @@ async function goChangePassword() {
       <button
         type="button"
         class="block w-full px-4 py-2 text-left text-sm text-gray-300 transition-colors hover:bg-gray-700 hover:text-white"
-        @click="goChangePassword"
+        @click.stop="goChangePassword"
       >
         修改密码
       </button>
